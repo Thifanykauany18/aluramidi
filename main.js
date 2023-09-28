2bfc2dcb694ad5e4a189f47b6f65d6d3dd624cf9
Thifanykauany18 <129421005+Thifanykauany18@users.noreply.github.com>
2023-09-28 14:33:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// Pom-----------------------------------------
-function tocaSomPom () {
+
+function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
 
- const ListaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
- 
+
 //enquanto
- white(contador < listaDeTeclas.length) {
+while (contador < listaDeTeclas.length) {
+   
+    listaDeTeclas(contador).onclick = tocaSomPom;
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+     contador = contador + 1;
 
-    contador = contador + 1;
-
-    console.log(contador);
- }
+     console.log(contador);
+}
